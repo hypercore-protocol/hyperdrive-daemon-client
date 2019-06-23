b03d2c6fe42c8bdc91f5cdd0291bc549adc88da0
Andrew Osheroff <andrewosh@gmail.com>
2019-06-23 16:31:38 +0200

    Cross-platform force-unmount

--- bin/fs/force-unmount.js
+++ bin/fs/force-unmount.js
@@ -1,3 +1,4 @@
+const os = require('os')
 const { execSync } = require('child_process')
 
 const chalk = require('chalk')
@@ -5,5 +6,9 @@ const chalk = require('chalk')
 exports.command = 'force-unmount'
 exports.desc = 'Forcibly unmount the root filesystem (useful if it was not cleanly unmounted).'
 exports.handler = function (argv) {
-  execSync('sudo umount -l /hyperdrive')
+  if (os.platform() === 'linux') {
+    execSync('sudo umount -l /hyperdrive')
+  } else if (os.platform() === 'darwin') {
+    execSync('sudo diskutil unmount /hyperdrive')
+  }
 }
