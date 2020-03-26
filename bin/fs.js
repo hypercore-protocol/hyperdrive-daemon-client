a628fb27c4491cde178cb8cd2cd9417f6dab02f5
Andrew Osheroff <andrewosh@gmail.com>
2020-03-26 13:55:08 +0100

    Removed fs subcommand

--- bin/fs.js
+++
@@ -1,9 +0,0 @@
-exports.command = 'fs <command>'
-exports.desc = 'Interact with FUSE-mounted hyperdrives.'
-exports.builder = function (yargs) {
-  return yargs
-    .commandDir('fs')
-    .demandCommand(1)
-    .help()
-}
-exports.handler = function (argv) {}
